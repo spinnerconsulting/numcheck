ddbfa9932e678073284ebe5d54781d3999dbeb33
Gregory M. Foreman <gforeman@spinnerconsulting.com>
2016-10-17 07:55:18 -0400

    insert test folder contents

---
+++ test/com/spinnerconsulting/InspectorGadgetTest.java
@@ -0,0 +1,14 @@
+package com.spinnerconsulting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InspectorGadgetTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
