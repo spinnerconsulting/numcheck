912fdecfaf8417b577a5e6752bfdc28e21c6379a
Peter <Peter@WenellCorei5>
2016-10-19 07:42:34 -0400

    New code

--- src/com/spinnerconsulting/InspectorGadget.java
+++ src/com/spinnerconsulting/InspectorGadget.java
@@ -9,6 +9,8 @@ public class InspectorGadget {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("===Start===");
+		System.out.println("Welcome to the InspectorGadget");
+		System.out.println("Push Version 19 Oct 7:38");
 		Excel e = new Excel();
 		e.doIt();
 		System.out.println("===End=====");
