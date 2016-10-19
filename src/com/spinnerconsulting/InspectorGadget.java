3095a5729485b47f4e09123170a79696bc466450
Gregory M. Foreman <gforeman@spinnerconsulting.com>
2016-10-19 07:46:12 -0400

    roundtriping

--- src/com/spinnerconsulting/InspectorGadget.java
+++ src/com/spinnerconsulting/InspectorGadget.java
@@ -10,6 +10,7 @@ public class InspectorGadget {
 	public static void main(String[] args) throws Exception {
 		System.out.println("===Start===");
 		System.out.println("Welcome to the InspectorGadget");
+		System.out.println("Greg");
 		System.out.println("Push Version 19 Oct 7:38");
 		Excel e = new Excel();
 		e.doIt();
