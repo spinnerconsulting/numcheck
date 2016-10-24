19345a8ef7be52b7782164dd87b0a4205c12a56d
olzhas.shaikenov <olzhas.shaikenov@192.168.99.1>
2016-10-23 20:23:35 -0400

    for loop with if statements, odd and even check

--- src/com/spinnerconsulting/Excel.java
+++ src/com/spinnerconsulting/Excel.java
@@ -25,61 +25,67 @@ public class Excel {
 		XSSFSheet sheet = wb.getSheetAt(0);
 
 		Iterator<Row> rowIterator = sheet.iterator();
-		
-		// Traversing over each row of XLSX file
-		while (rowIterator.hasNext()) {
-			Row row = rowIterator.next();
-		
-			// For each row, iterate through each columns
-			
-			Cell cell = row.getCell(2);
-		
-			boolean value;
-			
-			if (cell != null && cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
-				
-				String cellValue = NumberToTextConverter.toText(cell.getNumericCellValue());
-				Cell cell2 = row.createCell(3);
-				cell2.setCellValue("True");
-				
-				//System.out.println("CellValue " + cellValue + "\t");
-				//System.out.println(wd.valueExists("222"));
-				//value is null, cant figure out why.
-				//value = wd.valueExists(cellValue);
-				
-			}else if (cell != null && cell.getCellType() == Cell.CELL_TYPE_STRING && cell.toString() != "Number"){
-				String cellValue = (cell.getStringCellValue());
-				Cell cell2 = row.createCell(3);
-				cell2.setCellValue("True");
-			}else if(cell != null && cell.getCellType() == Cell.CELL_TYPE_BOOLEAN){
-				boolean cellValue =(cell.getBooleanCellValue());
-				Cell cell2 = row.createCell(3);
-				cell2.setCellValue("True");
-			}else if(cell != null && cell.getCellType() == Cell.CELL_TYPE_BLANK){
-				System.out.println("BLANK VALUE");
-			}else if(cell != null && cell.getCellType() == Cell.CELL_TYPE_ERROR){
-				byte cellValue =(cell.getErrorCellValue());
-				System.out.println(cellValue);
-			}else if(cell != null && cell.getCellType() == Cell.CELL_TYPE_FORMULA){
-				System.out.println("Formula");
-			}
-			
-			else{
-				System.out.println(cell);
-				
-			}
+		WebDriver wdTest = new WebDriver();
 
-			System.out.println("");
+		// Traversing over each row of XLSX file
+		//rowStart starts with 2, assuming 1st row is for the header info
 		
-		}
-		//
+		int rowStart= 2;
+		//Max row numbers in Excel is 65536 we will iterate over all of them. 
+		//Traditional for loop is used, instead of rowItearator that skips nulls and other values
 		
+		int rowEnd  = Math.max(1048576, sheet.getLastRowNum());
+		int i = 0;
+		for (int rowNum = 0; rowNum < rowEnd; rowNum++) {
+			   
+		       Row r = sheet.getRow(rowNum);
+		       
+		       if (r == null) {
+		          // This whole row is empty
+		          continue;
+		       }
+		       
+		       int lastColumn = Math.max(r.getLastCellNum(), 5);
+
+		       for (int cn = 0; cn < lastColumn; cn++) {
+		    	  
+		          Cell c = r.getCell(cn);
+		          if (c == null) {
+		             // The spreadsheet is empty in this cell
+		          }  else if (c.getCellType() == Cell.CELL_TYPE_NUMERIC){
+		        	  
+		        	  Cell cell2 = r.createCell(cn+1);
+		        	  cell2.setCellValue("True");
+
+
+					
+		          } else if (c.getCellType() == Cell.CELL_TYPE_STRING){
+		        	 
+		        	  
+		        			  
+		        			  
+		          } else if (c.getCellType() == Cell.CELL_TYPE_BLANK){
+		        	  continue;
+		          } else if (c.getCellType() == Cell.CELL_TYPE_BOOLEAN){
+		        	  continue;
+		          } else if (c.getCellType() == Cell.CELL_TYPE_ERROR){
+		        	  continue;
+		          } else if (c.getCellType() == Cell.CELL_TYPE_FORMULA){
+		        	  continue;
+		          }
+		          
+		          
+		      
+		       }
+		    }
+		
+
 		inp.close();
 
 		FileOutputStream os = new FileOutputStream("extras/demo.xlsx");
 		wb.write(os);
 		os.close();		
-		wd.close();
+		//wd.close();
 
 	}
 
@@ -95,6 +101,8 @@ public class Excel {
 	public static void main(String[] args) throws Exception {
 		Excel e = new Excel();
 		e.runQueries();
+		
+		
 	}
 
 }
